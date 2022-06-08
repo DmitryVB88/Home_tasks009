691fa088c12ee5bc7e5ca73467eba85473cbb123
Dima <dima.brashovyan@mail.ru>
2022-06-08 13:58:58 +0300

    Показать ряд натуральных чисел от n до m.

---
+++ First_task_numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы.
+
+Console.Clear();
+Console.Write("Введите число M: ");
+void Numbers (int n, int m)
+{
+    if(n < m) Console.Write($"{n}, ");
+    if(n == m) Console.Write($"{n}.");
+    if(n > m) return;
+    Numbers(n + 1, m);
+}
+Numbers (-5, 18);
+
+
