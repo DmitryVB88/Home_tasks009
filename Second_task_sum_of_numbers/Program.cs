f2c3ef49d9864d545d04ab7e7450151e64fd3a0f
Dima <dima.brashovyan@mail.ru>
2022-06-09 10:24:10 +0300

    посчитали сумму от m до n

---
+++ Second_task_sum_of_numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму элементов от M до N, N и M заданы.
+
+int SumOfNumbers (int m, int n)
+{ 
+    if(m == n) return n;
+    else return m + SumOfNumbers(m+1, n); 
+}
+
+Console.WriteLine(SumOfNumbers(1, 4));
